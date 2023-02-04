245acec2f129332e381182a0223511cea4966259
agorman75 <ajgorman84@gmail.com>
2023-02-04 12:11:02 -0500

    with folder

---
+++ new.py
@@ -0,0 +1,20 @@
+# for i in range(1,11):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+
+
+
+# def num(n):
+#     while n != 5:
+#         n = int(input('try again: '))
+#         if n == 5:
+#             print("Done!")
+
+# num(int(input("try: ")))
+
+import random
+print(random.randint(int(input("provide number: ")), 2))
