27c2b3d8cd33f4ce1cb10c12b115d8526f46e6e1
agorman75 <ajgorman84@gmail.com>
2023-02-28 16:34:50 -0500

    end of day work push

--- chatgpt_python/list_2.py
+++ chatgpt_python/list_2.py
@@ -7,9 +7,55 @@
 
 # sum_list(input("Provide a list of numbers: "))
 
-def word(words):
-    new = []
-    for [each] in words.split(','):
-        print(each)
+# def word(words):
+#     larger = ""
+#     for word in words:
+#         if len(words) > len(larger):
+#             larger = word
+#     print(larger)
 
-word(input("Provide a list of words: "))
+# word(input("Provide a list of words: ").split())
+
+# def rev(rev_list):
+#     rev_list = rev_list.split(',')
+#     rev_list.reverse()
+#     print(rev_list)
+
+# rev(input())
+
+# def even(num):
+#     even_each = 0
+#     for each in num:
+#         each = int(each)
+#         if each % 2 == 0:
+#             even_each += each
+#     print(even_each)
+
+# even(input().split(','))
+
+# def lists(l1, l2):
+#     l3 = []
+#     for each in l1:
+#         if each in l2 and each != ',':
+#             l3.append(each)
+#     print(l3)
+
+# lists(input(), input())
+
+# def lists(l1):
+#     new_list = []
+#     for each in l1.split(','):
+#         if len(each) >= 3:
+#             new_list.append(each)
+#     print(new_list)
+
+# lists(input())
+
+def long_words(words):
+    new_list = []
+    for word in words.split(','):
+        if len(word) >= 3:
+            new_list.append(word)
+    print(new_list)
+
+long_words(input("Enter a comma-separated list of words: "))
